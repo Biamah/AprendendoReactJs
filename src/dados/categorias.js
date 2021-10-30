aa999d2fe85ae69c581ba5d769d8a3151f159aab
Biamah <beatriz.souza.costa@outlook.com>
2021-10-30 15:05:42 -0300

    ajustando referencia do bind

--- src/dados/categorias.js
+++ src/dados/categorias.js
@@ -8,13 +8,19 @@ export default class Categorias {
     this._inscritos.push(func);
   }
 
+  desinscrever(func){
+    console.log(this._inscritos.length);
+    this._inscritos = this._inscritos.filter(f => f !== func);
+    console.log(this._inscritos.length);
+  }
+
   notificar(){
     this._inscritos.forEach(func => func(this.categorias));
   }
 
   adicionarCategoria(novaCategoria){
     this.categorias.push(novaCategoria);
-    this.notificar()
+    this.notificar();
   }
 
   // render() {
